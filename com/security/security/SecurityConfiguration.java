05f9ae295c81deb1f0e02539f8bf2a56562ffa90
TeaKljajin <teakljajin@yahoo.com>
2022-01-07 14:52:25 +0100

    Added notation

--- com/security/security/SecurityConfiguration.java
+++ com/security/security/SecurityConfiguration.java
@@ -3,11 +3,12 @@ package com.security.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
@@ -23,7 +24,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		
 	}
 	
-	@SuppressWarnings("deprecation")
 	@Bean
 	public PasswordEncoder getPasswordEncoder() {
 		return NoOpPasswordEncoder.getInstance() ;
